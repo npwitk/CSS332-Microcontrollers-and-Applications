122b9eb378ba552be7ed2fe9d61c951bf01c7be2
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-02-05 14:11:58 +0700

    Add Lab 1

---
+++ Lab 01/Exercise_00.asm
@@ -0,0 +1,12 @@
+;
+; 6622772422_Lab01.asm
+;
+; Created: 5/2/2568 13:00:32
+; Author : Nonprawich I.
+;
+
+
+LDI R20, 0x15
+LDI R22, 0x13
+STS 0x150, R22
+LDS R24, 0x150
---
+++ Lab 01/Exercise_01.asm
@@ -0,0 +1,20 @@
+;
+; 6622772422_Lab01.asm
+;
+; Created: 5/2/2568 13:00:32
+; Author : Nonprawich I.
+;
+
+
+		.EQU SUM = 0x300
+
+		.ORG 00
+		LDI R16, 0x25
+		LDI R17, $34
+		LDI R18, 0b00110001
+		ADD R16, R17
+		ADD R16, R18
+		LDI R17, 11
+		ADD R16, R17
+		STS SUM, R16
+HERE:	JMP HERE
