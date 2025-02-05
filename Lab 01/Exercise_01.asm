4e8b2edc6977bf730427e93e154fb78717fe3050
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-02-05 14:16:05 +0700

    Update Code

--- Lab 01/Exercise_01.asm
+++ Lab 01/Exercise_01.asm
@@ -1,20 +1,4 @@
-;
-; 6622772422_Lab01.asm
-;
-; Created: 5/2/2568 13:00:32
-; Author : Nonprawich I.
-;
-
-
-		.EQU SUM = 0x300
-
-		.ORG 00
-		LDI R16, 0x25
-		LDI R17, $34
-		LDI R18, 0b00110001
-		ADD R16, R17
-		ADD R16, R18
-		LDI R17, 11
-		ADD R16, R17
-		STS SUM, R16
-HERE:	JMP HERE
+LDI R16, 0xFF
+OUT DDRB, R16
+LDI R20, 0x5A
+OUT PORTB, R20
