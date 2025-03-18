fc517604f631fc5a35f7a893297d363f63bc63b5
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-03-18 14:52:28 +0700

    Add Lab 6

---
+++ Lab 06/Lab06_Exercise03.asm
@@ -0,0 +1,34 @@
+	.ORG 0
+	LDI R16, HIGH(RAMEND)
+	OUT SPH, R16
+	LDI R16, LOW(RAMEND)
+	OUT SPL, R16
+	SBI DDRD, 5 ; Set the pin PD5 as the output
+
+LOOP:	CBI PORTD, 5 ; Set the pin PD5 = 0
+	LDI R16, 0xD2
+HERE1:	CALL DELAY
+	DEC R16
+	BRNE HERE1
+
+	SBI PORTD, 5
+	LDI R16, 0xD2
+HERE2:	CALL DELAY
+	DEC R16
+	BRNE HERE2
+	RJMP LOOP
+
+DELAY:
+	LDI R20, 0x64
+	OUT TCNT0, R20 ; Set the initial value of Timer0
+	LDI R20, 0x00
+	OUT TCCR0A, R20 ; Set the normal mode
+	LDI R20, 0x05
+	OUT TCCR0B, R20 ; Pre-scaling 1024 & start the clock
+AGAIN:	SBIS TIFR0, 0 ; Check if TOV0 flag = 1?
+	RJMP AGAIN
+	LDI R20, 0x00
+	OUT TCCR0B, R20 ; Stop the clock
+	LDI R20, 0x01
+	OUT TIFR0, R20 ; Clear the TOV0 flag
+	RET ; Return to the main program!
---
+++ Lab 06/Lab06_Exercise04.asm
@@ -0,0 +1,36 @@
+	.ORG 0
+	LDI R16, HIGH(RAMEND)
+	OUT SPH, R16
+	LDI R16, LOW(RAMEND)
+	OUT SPL, R16
+	SBI DDRD, 5 ; Set the pin PD5 as the output
+
+LOOP:	CBI PORTD, 5 ; Set the pin PD5 = 0
+	LDI R16, 0xBA
+HERE1:	CALL DELAY
+	DEC R16
+	BRNE HERE1
+
+	SBI PORTD, 5 ; set pin PD5 = 1
+	LDI R16, 0xBA
+HERE2:	CALL DELAY
+	DEC R16
+	BRNE HERE2
+	RJMP LOOP
+
+DELAY:
+	LDI R20, 0x7D
+	OUT TCNT0, R20 ; Set the initial value of Timer0
+	LDI R20, 0xE9
+	OUT OCR0A, R20 ; Set the max value of Timer0
+	LDI R20, 0x02
+	OUT TCCR0A, R20 ; Set the CTC mode
+	LDI R20, 0x05
+	OUT TCCR0B, R20 ; Pre-scaling 1024 & start the clock
+AGAIN:	SBIS TIFR0, 1 ; Check if TOV0 flag = 1?
+	RJMP AGAIN
+	LDI R20, 0x00
+	OUT TCCR0B, R20 ; Stop the clock
+	LDI R20, 0x02
+	OUT TIFR0, R20 ; Clear the TOV0 flag
+	RET ; Return to the main program!
