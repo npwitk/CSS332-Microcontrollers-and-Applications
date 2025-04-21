048836592c874693cc15a95afce81db629fcc230
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-04-21 13:56:53 +0700

    Update

--- In-class Exercise/Apr21/Apr21.ino
+++ In-class Exercise/Apr21/Apr21.ino
@@ -1,6 +1,6 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(8, OUTPUT);
+  pinMode(4, OUTPUT);
   digitalWrite(8, HIGH);
 }
 
@@ -8,9 +8,9 @@ void loop() {
   int inValue = analogRead(A0);
   Serial.println(inValue);
   if (inValue > 600) {
-    digitalWrite(8, HIGH);
+    digitalWrite(4, HIGH);
   } else {
-    digitalWrite(8, LOW);
+    digitalWrite(4, LOW);
   }
   delay(1000);
 }
